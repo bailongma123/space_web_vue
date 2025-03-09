d421c14cd7b84da16fdf480b8106a4835efadc55
longcheng <37989497+bailongma123@users.noreply.github.com>
2025-03-09 17:01:37 +0800

    组件

--- vue_basic/vue_test/5组件.js
+++ vue_basic/vue_test/5组件.js
@@ -1,7 +1,9 @@
 Vue.component("toso-item", {
-    props:['todo1'],
-    //表示将  v-bind绑定的todo1 所指的对象中的 text 属性值渲染到 <li> 标签中。
-    template:'<li>{{todo1.text}}</li>'
+  props: ["todo1"],
+  //表示将  v-bind绑定的todo1 所指的对象中的 text 属性值渲染到 <li> 标签中。
+  //props 是数据的来源，template 是数据的展示方式。
+  //总结：props 是组件接收外部数据的接口。template 是组件使用这些数据渲染视图的地方。
+  template: "<li>{{todo1.text}}</li>",
 });
 
 var vm = new Vue({
